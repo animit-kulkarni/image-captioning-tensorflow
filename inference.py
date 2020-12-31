4b39d10b638a88174920a4fe16058e144292d0a9
animit-kulkarni <animitk@gmail.com>
2020-12-31 21:06:24 +0000

    added docstrings and comments for inference

--- inference.py
+++ inference.py
@@ -10,25 +10,22 @@ from model import CNN_Encoder, RNN_Decoder
 import utils
 from prepare_img_features import load_image
 
-
-
 class InstgramCaptioner:
 
-    def __init__(self, checkpoint_path, tokenizer_path, CONFIG, encoder=None, decoder=None):
+    def __init__(self, checkpoint_path, tokenizer_path, CONFIG):
+        """Load weights of encoder-decoder model from checkpoint. Load saved tokenizer.
+
+        Args:
+            checkpoint_path (str): path to directory containing checkpoints
+            tokenizer_path (str): path to pickle file storing tokenizer
+            CONFIG (CONFIG object): an object storing the configuration for package
+        """
 
         self.mobilenet_v2 = tf.keras.applications.MobileNetV2
         self.cnn_feature_model = self._reconfigure_cnn()
 
-        if encoder is not None:
-            self.encoder = encoder
-        else:
-            self.encoder = CNN_Encoder(CONFIG.EMBEDDING_SIZE)
-
-        if decoder is not None:
-            self.decoder = decoder
-        
-        else:
-            self.decoder = RNN_Decoder(CONFIG.EMBEDDING_SIZE, CONFIG.UNITS, CONFIG.VOCAB_SIZE)
+        self.encoder = CNN_Encoder(CONFIG.EMBEDDING_SIZE)
+        self.decoder = RNN_Decoder(CONFIG.EMBEDDING_SIZE, CONFIG.UNITS, CONFIG.VOCAB_SIZE)
 
         ckpt = tf.train.Checkpoint(encoder=self.encoder,
                                    decoder=self.decoder)
@@ -37,13 +34,21 @@ class InstgramCaptioner:
         ckpt.restore(ckpt_manager.latest_checkpoint)
 
         if ckpt_manager.latest_checkpoint:
-            print("**************** Restored from {}".format(ckpt_manager.latest_checkpoint))
+            print("******** Restored from {}".format(ckpt_manager.latest_checkpoint))
         else:
-            print("**************** Initializing from scratch.")
+            print("******** Initializing from scratch.")
 
         self.tokens_manager = pickle.load(open(tokenizer_path, 'rb'))
 
     def generate_caption(self, image_path):
+        """Use a CNN-GRU model to predict the caption to an image.
+
+        Args:
+            image_path (str): the path to the serialized image - png/jpg/jpeg.
+
+        Returns:
+            result: a list of strings in a sequence representing predicted caption.
+        """
 
         # max_length = 47 on this dataset
         max_length = self.tokens_manager.max_length
@@ -58,14 +63,13 @@ class InstgramCaptioner:
         decoder_input = tf.expand_dims([self.tokens_manager.tokenizer.word_index['<start>']], 0)
 
         result = []
-        for i in range(max_length):
+        for _ in range(max_length):
             predictions, hidden, attention_weights = self.decoder(decoder_input, features, hidden)
 
             predicted_id = np.argmax(predictions) 
             
             # we could use the code below instead to generate randomness in sentence creation - useful for production
-            # but not the testing here
-            #tf.random.categorical(predictions, 1, seed=42)[0][0].numpy()
+            # but not the testing here: tf.random.categorical(predictions, 1, seed=42)[0][0].numpy()
             
             result.append(self.tokens_manager.tokenizer.index_word[predicted_id])
 
@@ -79,6 +83,14 @@ class InstgramCaptioner:
 
 
     def _create_img_encoding(self, image_path):
+        """Encode the image using the CNN (e.g. MobileNetV2) and pass through a fully connected layer to embed the image's features.
+
+        Args:
+            image_path (str): path to the serialized img - png/jpg/jpeg
+
+        Returns:
+            features: a tensorflow Tensor object of dim [batch_size, cnn_feature_shape, embedding_dim] (e.g. [1, 49, 256])
+        """
         
         img = self._load_image(image_path)
         temp_input = tf.expand_dims(img, 0) # this is like saying batch_size = 1
@@ -88,8 +100,12 @@ class InstgramCaptioner:
 
         return features
 
-
     def _reconfigure_cnn(self):
+        """Reconfigures the MobileNetV2 architectire, removing the final layer (and ImageNet classification layer).
+
+        Returns:
+            tf.keras.Model: the reconfigured MobileNetV2 architecture.
+        """
 
         model = self.mobilenet_v2(include_top=False, weights='imagenet')
         new_input = model.input
@@ -99,55 +115,76 @@ class InstgramCaptioner:
 
 
     def _load_image(self, image_path):
+        """load_image function following the convention of keras preprocessing operations for consistency with training code.
+
+        Args:
+            image_path (str): path to serialized img - png/jpg/jpeg
+
+        Returns:
+            img: Tensor of image resized to (244, 244)
+        """
         img = tf.io.read_file(image_path)
         img = tf.image.decode_jpeg(img, channels=3)
         img = tf.image.resize(img, (224, 224))
         img = tf.keras.applications.imagenet_utils.preprocess_input(img)
         return img
 
-    def plot_img(self, idx, caption_filename_tuple_path, output_file='current_img.png'):
+    def test_img_from_mscoco(self, idx, caption_filename_tuple_path, output_file='current_img.png'):
+        """Test the model on an image from the downloaded dataset. This requires the caption_filename_tuple to have
+            been generated and pickled using utils.organise_data(). 
+
+            Example:
+
+                train_captions, img_name_vector = utils.organise_data()
+                caption_filename_tuple = list(zip(train_captions, img_name_vector))
+
+                with open(os.path.join(captions_dir,'caption_filename_tuple.pkl'), 'wb') as pickle_file:
+                    pickle.dump(caption_filename_tuple, pickle_file)
+
+                caption_bot = InstgramCaptioner(checkpoint_path, tokenizer_path, CONFIG)
+                caption_filename_tuple_path = os.path.join(CONFIG.CACHE_DIR_ROOT, 'mobilenet_v2_captions', 'caption_filename_tuple.pkl')
+
+                idx = 0 # take first img in dataset
+                caption_bot.test_img_from_mscoco(idx, caption_filename_tuple_path)
+            
+        Args:
+            idx (int): index the caption_filename_tuple to select an image for inference.
+            caption_filename_tuple_path (str): path to the caption_filename_tuple.
+            output_file (str, optional): path to output_file location. Defaults to 'current_img.png'.
+        """
 
         caption_filename_tuple = pickle.load(open(caption_filename_tuple_path, 'rb'))
-        
+        current_img_path = caption_filename_tuple[idx][1]
+
+        # remove <start> and <end> tokens and convert to string
         ground_truth_caption = ' '.join(caption_filename_tuple[idx][0].split(' ')[1:-1])
 
-        current_img_path = caption_filename_tuple[idx][1]
-        current_img = cv2.imread(current_img_path)
-
-
+        # forward pass on the model
         result, _ = self.generate_caption(current_img_path)
         gen_caption = ' '.join(result[:-1])
 
+        print(f'The caption PREDICTED by caption_bot'.center(80, '*'))
         print(gen_caption)
+        print(f'The original ground truth caption'.center(80, '*'))
         print(ground_truth_caption)
 
+        # cv2 operations to annotate the image with predicted and ground-truth captions
+        current_img = cv2.imread(current_img_path)
         cv2.rectangle(current_img, (15, 25), (current_img.shape[1] - 15, 85), (95, 95, 95), cv2.FILLED)
         cv2.putText(current_img, gen_caption, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,  (60, 30, 255), 1, cv2.LINE_AA)
         cv2.putText(current_img, ground_truth_caption, (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5,  (20, 240, 10), 1, cv2.LINE_AA)
-
         cv2.imwrite(output_file, current_img)
-
-
-
 
 if __name__ == '__main__':
 
     tokenizer_path = os.path.join(CONFIG.CACHE_DIR_ROOT, 'mobilenet_v2_captions', 'coco_tokenizer.pkl') 
     checkpoint_path = '/mnt/pythonfiles/models/mobilenet_v2_bahdanau/checkpoints/train/31122020-180918'
 
-
-    # image_path = os.path.join(CONFIG.IMAGES_DIR, os.listdir(CONFIG.IMAGES_DIR)[id_])
-
-    # current_img = cv2.imread(image_path)
-
     caption_bot = InstgramCaptioner(checkpoint_path, tokenizer_path, CONFIG)
-    # result, attention_plot = caption_bot.generate_caption(image_path)
     caption_filename_tuple_path = os.path.join(CONFIG.CACHE_DIR_ROOT, 'mobilenet_v2_captions', 'caption_filename_tuple.pkl')
 
-
     idx = int(sys.argv[1])
-
-    caption_bot.plot_img(idx, caption_filename_tuple_path)
+    caption_bot.test_img_from_mscoco(idx, caption_filename_tuple_path)
 
 
 
