53e55006bd607dfb39dae4230366d735399b6d12
animit-kulkarni <animitk@gmail.com>
2021-01-04 10:38:36 +0000

    had to change config class format to cope with wandb

--- experimentation/config.py
+++ experimentation/config.py
@@ -1,26 +1,35 @@
 class CONFIG:
-        # Paths
-        ANNOTATION_FILE = '/mnt/pythonfiles/training-sets/raw/MSCOCO/annotations/captions_train2014.json'
-        IMAGES_DIR = '/mnt/pythonfiles/training-sets/raw/MSCOCO/train2014'
-        CACHE_DIR_ROOT = '/mnt/pythonfiles/training-sets/interim'
-        CHECKPOINT_PATH = '/mnt/pythonfiles/models'
-        LOGS_DIR = '/mnt/pythonfiles/users/animit/InstagramCaptioner/.logs'
 
-        # Prepare image features parameters
-        NUMBER_OF_IMAGES = 6000
-        CNN_BACKBONE = 'inception_v3' #mobilenet_v2'
-        INCLUDE_CNN_IN_TRAINING = False
+        def __init__(self):
+                # Paths
+                self.ANNOTATION_FILE = '/mnt/pythonfiles/training-sets/raw/MSCOCO/annotations/captions_train2014.json'
+                self.IMAGES_DIR = '/mnt/pythonfiles/training-sets/raw/MSCOCO/train2014'
+                self.CACHE_DIR_ROOT = '/mnt/pythonfiles/training-sets/interim'
+                self.CHECKPOINT_PATH = '/mnt/pythonfiles/models'
+                self.LOGS_DIR = '/mnt/pythonfiles/users/animit/InstagramCaptioner/.logs'
 
-        # Training parameters
-        VOCABULARY_TOP_K = 5000
-        BATCH_SIZE = 512
-        BUFFER_SIZE = 1000
-        EMBEDDING_SIZE = 256
-        UNITS = 512
-        VOCAB_SIZE = VOCABULARY_TOP_K + 1
-        EPOCHS = 20
-        LEARNING_RATE = 0.001
+                # Prepare image features parameters
+                self.NUMBER_OF_IMAGES = 6000
+                self.CNN_BACKBONE = 'mobilenet_v2'
+                self.INCLUDE_CNN_IN_TRAINING = False
 
-        # Evaluation parameters
-        EVAL_BATCH_SIZE = 16
+                # Training parameters
+                self.VOCABULARY_TOP_K = 5000
+                self.BATCH_SIZE = 512
+                self.BUFFER_SIZE = 1000
+                self.EMBEDDING_SIZE = 256
+                self.UNITS = 512
+                self.VOCAB_SIZE = self.VOCABULARY_TOP_K + 1
+                self.EPOCHS = 20
+                self.LEARNING_RATE = 0.0035
+                self.RESUME_TRAINING = {'RESUME': False,
+                                        'CHECKPOINT_AND_MODEL_ID_PATH': '/mnt/pythonfiles/models/inception_v3_bahdanau/03012021-000041'}
+
+                # Evaluation parameters
+                self.EVAL_BATCH_SIZE = self.BATCH_SIZE
+
+                # Experiment tracking
+                self.WANDB = True
+                self.TENSORBOARD = False
+                
 
